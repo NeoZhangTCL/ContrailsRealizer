967baf60d3f7da4994b67c4d2b37d29886353d18
Tuochaolong Zhang <ztuochao@uwo.ca>
2016-04-24 09:52:15 -0400

    grayscalue function

--- ContrailsR.m
+++ ContrailsR.m
@@ -10,33 +10,31 @@ end
 function contractImage(imageInput)
 
 imageSize = size(imageInput);
-[red, green, blue] = getColorLayer(imageInput);
+garyValue = garyscale(imageInput);
 
 length = imageSize(1);
 width = imageSize(2);
-%redMean = mean(mean(red))*1.3;
-%blueMean = mean(mean(blue))*1.3;
-%greenMean = mean(mean(green))*1.3;
+garyAverage = mean(mean(garyValue))*1.3;
 
-map = java.util.HashMap;
-location = [1,2];
-color = [0, 255, 26];
-map.put(location, color);
+%map = java.util.HashMap;
+%color = 67;
+%location = [1 2];
+%map.put(color, location);
 
-answer = map.get([1,2]);
+%answer = map.get(67);
 
 for i = 1:length
     for j = 1:width
       
-%        if (red(i,j)<redMean||blue(i,j)<blueMean||green(i,j)<greenMean)
-%            imageInput(i,j,1)=0; 
-%            imageInput(i,j,2)=0; 
-%            imageInput(i,j,3)=0; 
-%        else
-%            imageInput(i,j,1)=255; 
-%            imageInput(i,j,2)=255; 
-%            imageInput(i,j,3)=255; 
-%        end
+        if (garyValue(i,j)<garyAverage)
+            imageInput(i,j,1)=0; 
+            imageInput(i,j,2)=0; 
+            imageInput(i,j,3)=0; 
+        else
+            imageInput(i,j,1)=255; 
+            imageInput(i,j,2)=255; 
+            imageInput(i,j,3)=255; 
+        end
     end
 end
 
@@ -45,8 +43,9 @@ imshow(imageInput);
 print output.jpg -djpeg
 end
 
-function [red, green, blue] = getColorLayer(imageInput)
+function [intensity] = garyscale(imageInput)
     red=squeeze(imageInput(:,:,1));
     green=squeeze(imageInput(:,:,2));
-    blue=squeeze(imageInput(:,:,3));    
+    blue=squeeze(imageInput(:,:,3)); 
+    intensity = 0.2989.*red + 0.5870.*green + 0.1140.*blue;
 end
